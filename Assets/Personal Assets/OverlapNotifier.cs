d21c50b6142c2d10b7d3ad548ff6ae2e209f4199
George <georgejr1111@gmail.com>
2017-03-05 19:22:13 -0800

    - Added missing files.

---
+++ Assets/Personal Assets/OverlapNotifier.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OverlapNotifier : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("OverlapNotifier started!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("OverlapNotifier Tick");
+    }
+    // NOTE: Capital "O" in OnTriggerEnter
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "MainCamera")
+        {
+            Debug.Log("Camera overlapped me!");
+        }
+        else
+        {
+            Debug.Log("Something overlapped me!");
+            SceneManager.LoadScene("VRTrivia");
+        }
+    }
+}
