5a9451308d5162a7deb2425d6d4d2678cbbbb757
George <georgejr1111@gmail.com>
2017-03-14 20:35:32 -0700

    - Creation of TeleporterScript (Prefab usage). - Browsers in Photosynthesis and Carbon Cycling

---
+++ Assets/Personal Assets/TeleporterScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TeleporterScript : MonoBehaviour
+{
+    public string Destination = "VRTrivia";
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("OverlapNotifier started!");
+    }
+    // NOTE: Capital "O" in OnTriggerEnter
+    void OnTriggerEnter(Collider other)
+    {
+        {
+            Debug.Log("Something overlapped me!");
+            SceneManager.LoadScene(Destination);
+        }
+    }
+}
